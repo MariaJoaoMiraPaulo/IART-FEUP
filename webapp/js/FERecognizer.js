06bedec21e646476e019939c1834be7b786f2745
Pedro Costa <pedrodc23@gmail.com>
2017-04-24 18:03:13 +0100

    Just do it

--- webapp/js/FERecognizer.js
+++ webapp/js/FERecognizer.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function() {
   original_data();
 
 });
@@ -8,86 +8,80 @@ var data = [];
 var labels = [];
 
 var net; // declared outside -> global variable in window scope
-function start() {
+
+function trainNetwork() {
   // this gets executed on startup
   //...
   net = new convnetjs.Net();
 
   var layer_defs = [];
-// input layer of size 1x1x20, 20 pontos de input (12-distancias + 8 angulos)
-layer_defs.push({type:'input', out_sx:1, out_sy:1, out_depth:20});
-// some fully connected layers
-layer_defs.push({type:'fc', num_neurons:20, activation:'relu'});
-//layer_defs.push({type:'fc', num_neurons:20, activation:'sigmoid'});
-// a softmax classifier predicting probabilities for two classes: 0,1
-layer_defs.push({type:'softmax', num_classes:10});
+  // input layer of size 1x1x20, 20 pontos de input (12-distancias + 8 angulos)
+  layer_defs.push({
+    type: 'input',
+    out_sx: 1,
+    out_sy: 1,
+    out_depth: 20
+  });
+  // some fully connected layers
+  layer_defs.push({
+    type: 'fc',
+    num_neurons: 20,
+    activation: 'relu'
+  });
+  //layer_defs.push({type:'fc', num_neurons:20, activation:'sigmoid'});
+  // a softmax classifier predicting probabilities for two classes: 0,1
+  layer_defs.push({
+    type: 'softmax',
+    num_classes: 10
+  });
 
-// create a net out of it
-var net = new convnetjs.Net();
-net.makeLayers(layer_defs);
+  // create a net out of it
+  var net = new convnetjs.Net();
+  net.makeLayers(layer_defs);
 
-// the network always works on Vol() elements. These are essentially
-// simple wrappers around lists, but also contain gradients and dimensions
-var x = new convnetjs.Vol(1,1,20);
-
-var probability_volume = net.forward(x);
-console.log('probability that x is class 0: ' + probability_volume.w[0]);
-// prints 0.50101
-
-var trainer = new convnetjs.Trainer(net, {learning_rate:0.01, l2_decay:0.001});
-trainer.train(x, 0);
-
-var probability_volume2 = net.forward(x);
-console.log('probability that x is class 0: ' + probability_volume2.w[0]);
-// prints 0.50374
-
-}
-
-function update(){
+  var trainer = new convnetjs.Trainer(net, {
+    learning_rate: 0.01,
+    l2_decay: 0.001
+  });
   // forward prop the data
-
-  var netx = new convnetjs.Vol(1,1,20);
+  var netx = new convnetjs.Vol(1, 1, 20);
   avloss = 0.0;
-  N = data.length
-  for(var iters=0;iters<50;iters++) {
-    for(var ix=0;ix<N;ix++) {
-      netx.w = data[ix];
-      var stats = trainer.train(netx, labels[ix]);
-      avloss += stats.loss;
-      console.log(avloss);
-    }
+  N = data.length;
+  for (var ix = 0; ix < N; ix++) {
+    netx.w = data[ix];
+    var stats = trainer.train(netx, labels[ix]);
+    avloss += stats.loss;
+    console.log(avloss);
   }
-  avloss /= N*iters;
-
 }
 
-function original_data(){
-/*  data = [];
-  labels = [];*/
+function original_data() {
+  /*  data = [];
+    labels = [];*/
   /*files = ['a_affirmative_datapoints.json','a_conditional_datapoints.json','a_doubt_question_datapoints.json','a_emphasis_datapoints.json',
 'a_negative_datapoints.json','a_relative_datapoints.json','a_topics_datapoints.json','a_wh_question_datapoints.json','a_yn_question_datapoints.json',
 'b_affirmative_datapoints.json','b_conditional_datapoints.json','b_doubt_question_datapoints.json','b_emphasis_datapoints.json','b_negative_datapoints.json',
 'b_relative_datapoints.json','b_topics_datapoints.json','b_wh_question_datapoints.json','b_yn_question_datapoints.json'];
 */
   files = ['a_affirmative_datapoints.json'];
-  for(file of files){
+  for (file of files) {
     console.log('Loading file ' + file);
-    load_JSON(file,prepare_data);
+    load_JSON(file, prepare_data);
   }
 
-  setTimeout(function(){
+  setTimeout(function() {
     console.log(data);
     console.log(labels);
   }, 3000);
 }
 
-function load_JSON(file,callback) {
+function load_JSON(file, callback) {
 
   var xobj = new XMLHttpRequest();
   xobj.overrideMimeType("application/json");
   xobj.open('GET', 'JSON/' + file, true);
 
-  xobj.onreadystatechange = function () {
+  xobj.onreadystatechange = function() {
     if (xobj.readyState == 4 && xobj.status == "200") {
 
       // .open will NOT return a value but simply returns undefined in async mode so use a callback
@@ -98,21 +92,20 @@ function load_JSON(file,callback) {
   xobj.send(null);
 }
 
-function prepare_data(response){
+function prepare_data(response) {
   jsonResponse = JSON.parse(response);
 
   console.log(jsonResponse.length);
 
   var label;
 
-  for(line of jsonResponse){
+  for (line of jsonResponse) {
     var lineData = [];
-    for(element in line){
+    for (element in line) {
 
-      if(element == 'target'){
-        label=line[element];
-      }
-      else if(element != 'index'){
+      if (element == 'target') {
+        label = line[element];
+      } else if (element != 'index') {
         lineData.push(line[element]);
       }
     }
