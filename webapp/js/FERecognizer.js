07512563932ad7e7bc7392565178aca0de313d25
Pedro Costa <pedrodc23@gmail.com>
2017-04-24 17:27:33 +0100

    Working on the network

--- webapp/js/FERecognizer.js
+++ webapp/js/FERecognizer.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
   original_data();
+
 });
 
-
+var N;
 var data = [];
 var labels = [];
 
@@ -13,11 +14,11 @@ function start() {
   net = new convnetjs.Net();
 
   var layer_defs = [];
-// input layer of size 1x1x2 (all volumes are 3D)
+// input layer of size 1x1x20, 20 pontos de input (12-distancias + 8 angulos)
 layer_defs.push({type:'input', out_sx:1, out_sy:1, out_depth:20});
 // some fully connected layers
 layer_defs.push({type:'fc', num_neurons:20, activation:'relu'});
-layer_defs.push({type:'fc', num_neurons:20, activation:'relu'});
+//layer_defs.push({type:'fc', num_neurons:20, activation:'sigmoid'});
 // a softmax classifier predicting probabilities for two classes: 0,1
 layer_defs.push({type:'softmax', num_classes:10});
 
@@ -27,8 +28,7 @@ net.makeLayers(layer_defs);
 
 // the network always works on Vol() elements. These are essentially
 // simple wrappers around lists, but also contain gradients and dimensions
-// line below will create a 1x1x2 volume and fill it with 0.5 and -1.3
-var x = new convnetjs.Vol([0.5, -1.3]);
+var x = new convnetjs.Vol(1,1,20);
 
 var probability_volume = net.forward(x);
 console.log('probability that x is class 0: ' + probability_volume.w[0]);
@@ -43,14 +43,33 @@ console.log('probability that x is class 0: ' + probability_volume2.w[0]);
 
 }
 
+function update(){
+  // forward prop the data
+
+  var netx = new convnetjs.Vol(1,1,20);
+  avloss = 0.0;
+  N = data.length
+  for(var iters=0;iters<50;iters++) {
+    for(var ix=0;ix<N;ix++) {
+      netx.w = data[ix];
+      var stats = trainer.train(netx, labels[ix]);
+      avloss += stats.loss;
+      console.log(avloss);
+    }
+  }
+  avloss /= N*iters;
+
+}
+
 function original_data(){
 /*  data = [];
   labels = [];*/
-  files = ['a_affirmative_datapoints.json','a_conditional_datapoints.json','a_doubt_question_datapoints.json','a_emphasis_datapoints.json',
+  /*files = ['a_affirmative_datapoints.json','a_conditional_datapoints.json','a_doubt_question_datapoints.json','a_emphasis_datapoints.json',
 'a_negative_datapoints.json','a_relative_datapoints.json','a_topics_datapoints.json','a_wh_question_datapoints.json','a_yn_question_datapoints.json',
 'b_affirmative_datapoints.json','b_conditional_datapoints.json','b_doubt_question_datapoints.json','b_emphasis_datapoints.json','b_negative_datapoints.json',
 'b_relative_datapoints.json','b_topics_datapoints.json','b_wh_question_datapoints.json','b_yn_question_datapoints.json'];
-
+*/
+  files = ['a_affirmative_datapoints.json'];
   for(file of files){
     console.log('Loading file ' + file);
     load_JSON(file,prepare_data);
@@ -84,20 +103,20 @@ function prepare_data(response){
 
   console.log(jsonResponse.length);
 
-  var lineData = [];
-  var lineLabels = [];
+  var label;
 
   for(line of jsonResponse){
+    var lineData = [];
     for(element in line){
 
       if(element == 'target'){
-        lineLabels.push(line[element]);
+        label=line[element];
       }
       else if(element != 'index'){
         lineData.push(line[element]);
       }
     }
     data.push(lineData);
-    labels.push(lineLabels);
+    labels.push(label);
   }
 }
