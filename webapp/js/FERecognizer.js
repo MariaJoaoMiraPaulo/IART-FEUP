e704091e4d68d7b22bc8eb2f2c8fd11e4b2b5671
Pedro Costa <pedrodc23@gmail.com>
2017-04-28 21:14:49 +0100

    Separating expressions

--- webapp/js/FERecognizer.js
+++ webapp/js/FERecognizer.js
@@ -18,16 +18,17 @@ var l2_decay;
 var batch_size;
 var training_method;
 var momentum;
-var lossWindows;
-var trainAccWindows;
-var testAccWindows;
+var lossWindows = [];
+var trainAccWindows = [];
+var testAccWindows = [];
 var lossGraph, trainGraph, testGraph;
 var activation_function_output;
 var number_of_hidden_layers;
 var activation_function;
 var netx;
 var avloss = 0;
-
+var legend = ['neutra', 'affirmative', 'conditional', 'doubt_question', 'emphasis', 'negative', 'relative', 'topics', 'wh_question', 'yn_question'];
+var stats;
 var net = new convnetjs.Net(); // declared outside -> global variable in window scope 
 
 $(document).ready(function () {
@@ -35,6 +36,8 @@ $(document).ready(function () {
 });
 
 function initNetwork() {
+  //Shuffles the data while keeping targets synced
+  //shuffleData();
   var layer_defs = [];
   setPreferences();
   initGraphs();
@@ -56,7 +59,7 @@ function initNetwork() {
     });
   }
 
-/* Output layer */
+  /* Output layer */
   layer_defs.push({
     type: activation_function_output,
     num_classes: 10
@@ -65,16 +68,18 @@ function initNetwork() {
   net.makeLayers(layer_defs);
   setPreferences();
   initTrainer();
+  //Start with a trained network
+  //trainNetwork();
   intervalId = setInterval(load_and_step, 0);
 }
 
 
-function setPreferences(){
+function setPreferences() {
 
   activation_function_output = $('#activation-function-for-output-layer').val();
   number_of_hidden_layers = $('#hidden-layers').val();
   activation_function = $('#activation-function').val();
-  learning_rate = $('#learning-rate').val();  
+  learning_rate = $('#learning-rate').val();
   l1_decay = $('#l1-decay').val();
   l2_decay = $('#l2-decay').val();
   batch_size = $('#batch-size').val();
@@ -84,14 +89,11 @@ function setPreferences(){
 
 function initGraphs() {
 
-  lossWindows = new cnnutil.Window(800);
-  trainAccWindows = new cnnutil.Window(800);
-  testAccWindows = new cnnutil.Window(800);
-  lossGraph, trainGraph, testGraph;
-
-  if (training_method)
-    legend = [training_method];
-  else legend = ['adadelta'];
+  for (var i = 0; i < legend.length; i++) {
+    lossWindows.push(new cnnutil.Window(800));
+    trainAccWindows.push(new cnnutil.Window(800));
+    testAccWindows.push(new cnnutil.Window(800));
+  }
 
   lossGraph = new cnnvis.MultiGraph(legend);
   trainGraph = new cnnvis.MultiGraph(legend);
@@ -222,9 +224,9 @@ function load_and_step() {
   train_length = train_data.length;
   var losses = [];
   var trainacc = [];
-  testacc = [];
+  var testacc = [];
 
-  if (testIteraction > test_length) 
+  if (testIteraction > test_length)
     testIteraction = 1;
 
   if (trainIteraction > train_length)
@@ -236,29 +238,37 @@ function load_and_step() {
   netx.w = train_data[trainIteraction];
   var stats = trainer.train(netx, train_labels[trainIteraction]);
   avloss = stats.loss;
-  
-  var yhat = net.getPrediction();
-  trainAccWindows.add(yhat === train_labels[trainIteraction] ? 1.0 : 0.0);
 
-  lossWindows.add(avloss);
+  var yhat = net.getPrediction();
+  trainAccWindows[train_labels[testIteraction]].add(yhat === train_labels[trainIteraction] ? 1.0 : 0.0);
+
+  lossWindows[train_labels[trainIteraction]].add(avloss);
 
   var x = new convnetjs.Vol(1, 1, 20);
   x.w = test_data[testIteraction];
   var scores = net.forward(x); // pass forward through network
   var yhat_test = net.getPrediction();
-  testAccWindows.add(yhat_test === test_labels[testIteraction] ? 1.0 : 0.0);
+  testAccWindows[test_labels[testIteraction]].add(yhat_test === test_labels[testIteraction] ? 1.0 : 0.0);
   if (yhat_test === test_labels[testIteraction]) {
     console.log("Correct: Result  " + yhat_test + " Expected " + test_labels[testIteraction]);
     console.log(scores.w);
   } else {
-     console.log("False: Result " + yhat_test + " Expected " + test_labels[testIteraction]);
+    console.log("False: Result " + yhat_test + " Expected " + test_labels[testIteraction]);
     console.log(scores.w);
   }
 
+  for (var i = 0; i < legend.length; i++) {
+    if (lossWindows[i].get_average() != -1) {
+      losses.push(lossWindows[i].get_average());
+    }
+    if (lossWindows[i].get_average() != -1) {
+      trainacc.push(trainAccWindows[i].get_average());
+    }
+    if (lossWindows[i].get_average() != -1) {
+      testacc.push(testAccWindows[i].get_average());
+    }
 
-  losses.push(lossWindows.get_average());
-  trainacc.push(trainAccWindows.get_average());
-  testacc.push(testAccWindows.get_average());
+  }
 
   lossGraph.add(step_num, losses);
   lossGraph.drawSelf(document.getElementById("lossgraph"));
@@ -276,10 +286,10 @@ function saveNetwork() {
   document.getElementById("network_data").innerHTML = "";
 
   /* network outputs all of its parameters into json object*/
-  var json = net.toJSON(); 
+  var json = net.toJSON();
 
   /* the entire object is now simply string. You can save this somewhere */
-  var netwok_data = JSON.stringify(json); 
+  var netwok_data = JSON.stringify(json);
 
   console.log("Saving network ... ");
   document.getElementById("network_data").innerHTML = netwok_data;
@@ -292,8 +302,8 @@ function loadNetwork() {
   console.log("Loading network ... ");
 
   /* create an empty network */
-  net = new convnetjs.Net(); 
-  net.fromJSON(json); 
+  net = new convnetjs.Net();
+  net.fromJSON(json);
   /* load all parameters from JSON */
   initGraphs();
 
@@ -309,4 +319,53 @@ function loadNetwork() {
 function stopNetwork() {
   console.log("Stoping Network...");
   clearInterval(intervalId);
+}
+
+function trainNetwork() {
+  netx = new convnetjs.Vol(1, 1, 20);
+
+  for (var x = 0; x < 100; x++) {
+    for (var i = 0; i < test_data.length; i++) {
+      netx.w = train_data[trainIteraction];
+      stats = trainer.train(netx, train_labels[trainIteraction]);
+      avloss = stats.loss;
+      console.log(avloss);
+    }
+  }
+}
+
+function shuffleData() {
+  let trainCounter = train_data.length;
+  let testCounter = test_data.length
+  // While there are elements in the array
+  while (trainCounter > 0) {
+    // Pick a random index
+    let index = Math.floor(Math.random() * trainCounter);
+
+    // Decrease counter by 1
+    trainCounter--;
+
+    // And swap the last element with it
+    let temp1 = train_data[trainCounter];
+    let temp2 = train_labels[trainCounter];
+    train_data[trainCounter] = train_data[index];
+    train_data[index] = temp1;
+    train_labels[trainCounter] = train_labels[index];
+    train_labels[index] = temp2;
+  }
+  while (testCounter > 0) {
+    // Pick a random index
+    let index = Math.floor(Math.random() * testCounter);
+
+    // Decrease counter by 1
+    testCounter--;
+
+    // And swap the last element with it
+    let temp1 = test_data[testCounter];
+    let temp2 = test_labels[testCounter];
+    test_data[testCounter] = test_data[index];
+    test_data[index] = temp1;
+    test_labels[testCounter] = test_labels[index];
+    test_labels[index] = temp2;
+  }
 }
